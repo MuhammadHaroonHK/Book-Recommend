bf16fbc7ec69442afa7a78e2a33401f594641051
MuhammadHaroonHK <haroonhk059@gmail.com>
2026-05-09 11:02:18 +0500

    Now model is connected with main python.

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,43 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from model import recommend_books
 
 app = FastAPI()
 
+# =========================
+# CORS
+# =========================
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# =========================
+# HOME
+# =========================
+
 @app.get("/")
 def home():
+
     return {
         "message": "Book Recommendation API Running"
+    }
+
+# =========================
+# RECOMMEND API
+# =========================
+
+@app.get("/recommend/{book_name}")
+def recommend(book_name: str):
+
+    recommendations = recommend_books(book_name)
+
+    return {
+        "book": book_name,
+        "recommendations": recommendations
     }
